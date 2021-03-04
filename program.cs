7b05c2767a6d7f57ccefea5a5e24683d65f9d846
dahdi <62443126+dahdi@users.noreply.github.com>
2021-03-04 17:00:06 +0000

    made the readline a variable and converted the string to an array of characters

--- program.cs
+++ program.cs
@@ -9,7 +9,8 @@ namespace CaesarCipher
       char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
       
       Console.Write("Please enter a secret code: ");
-      Console.ReadLine();
+      string msgString = Console.ReadLine();
+      char[] msgArray = msgString.ToCharArray();
     }
   }
 }
